6c3c002cf6eba93567a2f4265074e22e6c739b92
hejl <hejl@bmsh.com>
2018-11-27 18:42:51 +0800

    add web file 1127

--- web_py/static/main.js
+++ web_py/static/main.js
@@ -1,41 +1,34 @@
 function mainf( Vue ) {
   
 
-
-var global_data = {
-  hello: "21",
-  active_idx: 0
-}
-
-// 2. 定义路由
-const routes = [
-  { path: '/main', component: { template: '#id_main' } },
-  { path: '/conf', component: { template: '#id_conf' } },
-  { path: '/log', component: { template: '#id_log' } },
-  { path: '/about', component: { template: '#id_about' } }
-]
-
-// 3. 创建 router 实例，然后传 `routes` 配置
-const router = new VueRouter({
-  routes // (缩写) 相当于 routes: routes
-})
-
-// 4. 创建和挂载根实例。
 const app = new Vue({
-  data: global_data,
-  methods: {
-    nat_click: function(idx) {
-      console.log("cliedk " + idx);
-      active_idx = idx;
-    },
-    active_classset: function(idx) {
-      if (typeof(active_idx) == "undefined") {
-        return 0 === idx ? "active" : "";
-      }
-      return idx === active_idx ? "active" : "";
-    }
+  data: {
+    tdata: 123,
+    maindata: [],
+    colm_checked: ['svrid', 'svrname']
   },
-  router
+
+  methods: {
+    get_maindata : function(id) {
+        this.$http.get('/clidata/' + id).then(function(res){
+            var result = res.body.code
+            if (0 != result) {
+                alert('获取失败：'+res.body.desc)
+            } else {
+                this.maindata = res.body.data;
+            }
+        }, function(){
+            alert('请求/svrall失败处理');
+        })
+    }
+
+  },
+
+  created: function() {
+    this.get_maindata(0);
+  }
+
+  
 }).$mount('#app')
 
 }
